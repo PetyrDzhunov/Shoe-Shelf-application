fa1324dbc8ca3bf91b50788d7a2548c9bc6a5d79
PetyrDzhunov <petar.djunov@abv.bg>
2021-07-17 13:28:34 +0300

    fixed-some-issues

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = Sammy('#app', function() {
+
+    this.use('Handlebars', 'hbs');
+    //Home routes
+    this.get('/home', function() {
+        this.partial('../templates/homeGuest.hbs');
+    });
+
+    //User routes
+    this.get('/register', function() {
+        this.partial('./templates/register.hbs')
+    });
+
+    this.get('/login', function() {
+        this.partial('./templates/login.hbs')
+    });
+
+    //Offers routes
+    this.get('/create-offer', function() {
+        this.partial('./templates/createOffer.hbs');
+    });
+
+    this.get('/edit-offer', function() {
+        this.partial('./templates/editOffer.hbs');
+    });
+
+    this.get('/details', function() {
+        this.partial('./templates/details.hbs');
+    });
+
+
+});
+
+(() => {
+    app.run('/home');
+})();
