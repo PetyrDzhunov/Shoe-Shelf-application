3ceae7b7f37d510425e8be5780d9c5a9a44fca6a
PetyrDzhunov <petar.djunov@abv.bg>
2021-07-17 21:33:13 +0300

    message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = Sammy('#app', function() {
     this.use('Handlebars', 'hbs');
     //Home routes
     this.get('/home', function() {
-        this.partial('/templates/homeGuest.hbs');
+        this.partial('./templates/home.hbs');
     });
 
     //User routes
